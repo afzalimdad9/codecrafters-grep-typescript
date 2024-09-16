eb4ca94be37f0c4a75fc26582f196fde0fa22861
afzalimdad9 <afzalimdad9@gmail.com>
2024-09-16 10:26:47 +0500

    codecrafters submit [skip ci]

--- app/main.ts
+++ app/main.ts
@@ -3,11 +3,29 @@ const pattern = args[3];
 
 const inputLine: string = await Bun.stdin.text();
 
+const isDigit = (c: string): boolean => {
+  const code = c.charCodeAt(0);
+  return code >= 48 && code <= 57;
+};
+const isAlphabetic = (c: string): boolean => {
+  const code = c.charCodeAt(0);
+  return (code >= 65 && code <= 90) || (code >= 97 && code <= 122);
+};
+const isUnderscore = (c: string): boolean => {
+  return c === "_";
+};
+
+
 function matchPattern(inputLine: string, pattern: string): boolean {
   if (pattern.length === 1) {
     return inputLine.includes(pattern);
   } else if (pattern === "\\d") {
     return /\d/g.test(inputLine);
+  } else if (pattern === "\\w") {
+    console.log(inputLine, "inputLine");
+    for (const c of inputLine) {
+      return isDigit(c) || isAlphabetic(c) || isUnderscore(c);
+    }
   } else {
     throw new Error(`Unhandled pattern: ${pattern}`);
   }
